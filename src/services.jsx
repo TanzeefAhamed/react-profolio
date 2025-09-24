3dca5977705547414198319312b76226059519fc
Tanzeef Ahamed <mahame10@my.centennialcollege.ca>
2025-09-24 12:26:43 -0400

    First commit - portfolio site

---
+++ src/services.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+// This is our Services page
+function Services() {
+  return (
+    <div>
+      <h1>My Services</h1>
+      <p>Here are some things I can do:</p>
+
+      <div>
+        <h2>Web Development</h2>
+        <p>I can make websites using React, HTML, and CSS.</p>
+      </div>
+
+      <div>
+        <h2>Health Informatics</h2>
+        <p>I can analyze raw health data, clean, and organize them.</p>
+      </div>
+
+      <div>
+        <h2>Database Design</h2>
+        <p>I understand the basics of storing and organizing data.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Services;
